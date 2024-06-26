1921ff54c2ca29d4ef11de6c54f2d02484703768
SHUBHAM VASHISHT <115337099+Shubham04vashisht@users.noreply.github.com>
2024-06-26 18:52:51 +0530

    Update NoteState.js

--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -1,7 +1,7 @@
 import NoteContext from "./NoteContext";
 import { useState } from "react";
 const NoteState=(props)=>{
-  const host="http://localhost:5000"
+  const host="https://inotebook-api-three.vercel.app"
     const tempNotes=[]
     const [notes,setNotes]=useState(tempNotes);
 
